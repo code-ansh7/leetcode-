6c4f99a26b45e721aa017372be6a0395858ec06f
Ansh Rastogi <anshrastogi9236920967@gmail.com>
2026-05-26 12:07:58 +0545

    Create cheakPalindrom.c

---
+++ cheakPalindrom.c
@@ -0,0 +1,19 @@
+ bool isPalindrome(int x) {
+
+    int original = x;
+    long long reverse = 0;
+    int digit;
+
+    if(x < 0)
+        return false;
+    while(x > 0) {
+        digit = x % 10;
+        reverse = reverse * 10 + digit;
+        x = x / 10;
+    }
+
+    if(original == reverse)
+        return true;
+    else
+        return false;
+}
